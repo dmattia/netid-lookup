fb97625fa047b2e51e55e0a332b172cf6713ab63
dmattia <dmattia@nd.edu>
2016-02-24 16:02:51 -0500

    Added twilio response

--- app.py
+++ app.py
@@ -1,9 +1,30 @@
-from flask import Flask
+import Flask, request, redirect
+import twilio.twiml
+ 
 app = Flask(__name__)
  
-@app.route("/")
-def hello():
-    return "Hello World!"
+# Try adding your own number to this list!
+callers = {
+	"+16124378532": "David",
+    "+14158675309": "Curious George",
+    "+14158675310": "Boots",
+    "+14158675311": "Virgil",
+}
+ 
+@app.route("/", methods=['GET', 'POST'])
+def hello_monkey():
+    """Respond and greet the caller by name."""
+ 
+    from_number = request.values.get('From', None)
+    if from_number in callers:
+        message = callers[from_number] + ", thanks for the message!"
+    else:
+        message = "Monkey, thanks for the message!"
+ 
+    resp = twilio.twiml.Response()
+    resp.message(message)
+ 
+    return str(resp)
  
 if __name__ == "__main__":
     app.run(debug=True)
